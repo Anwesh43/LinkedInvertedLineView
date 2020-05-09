bd8a360ce48d91deff5ad2313c054b7c38d60363
Anwesh43 <anweshthecool0@gmail.com>
2020-05-10 04:54:15 +0530

    using renderer in view

--- invertedlineview/src/main/java/com/anwesh/uiprojects/invertedlineview/InvertedLineView.kt
+++ invertedlineview/src/main/java/com/anwesh/uiprojects/invertedlineview/InvertedLineView.kt
@@ -63,14 +63,16 @@ fun Canvas.drawILNode(i : Int, scale : Float, paint : Paint) {
 
 class InvertedLineView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -209,6 +211,5 @@ class InvertedLineView(ctx : Context) : View(ctx) {
                 animator.start()
             }
         }
-
     }
 }
