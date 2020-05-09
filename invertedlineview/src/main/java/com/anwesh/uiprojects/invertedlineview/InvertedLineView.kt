ba8e27f3acd3408e5a723a11a14ad8321daab82c
Anwesh43 <anweshthecool0@gmail.com>
2020-05-10 04:20:50 +0530

    adding constants and libraries

--- invertedlineview/src/main/java/com/anwesh/uiprojects/invertedlineview/InvertedLineView.kt
+++ invertedlineview/src/main/java/com/anwesh/uiprojects/invertedlineview/InvertedLineView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.invertedlineview
 /**
  * Created by anweshmishra on 10/05/20.
  */
+
+import android.view.View
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.view.MotionEvent
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val parts : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#212121")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
